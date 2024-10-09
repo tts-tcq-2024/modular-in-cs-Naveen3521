a28e7df42b7d4af4d58941f8a51ea033fe7cc53a
Naveen3521 <87026171+Naveen3521@users.noreply.github.com>
2024-10-09 20:57:52 +0530

    Update ColorMap.cs

--- ColorMap.cs
+++ ColorMap.cs
@@ -1,1 +1,28 @@
+using System.Drawing;
 
+namespace TelCo.ColorCoder
+{
+  public static class ColorMap
+  {
+    private static readonly Color[] majorColor = new Color[]
+    {
+            Color.White, 
+            Color.Red, 
+            Color.Black, 
+            Color.Yellow, 
+            Color.Violet 
+    };
+
+    private static readonly Color[] minorColors = new Color[] 
+    { 
+            Color.Blue, 
+            Color.Orange, 
+            Color.Green, 
+            Color.Brown, 
+            Color.SlateGray 
+    };
+
+    public static Color[] MajorColors => (Color[])majorColors.Clone();
+    public static Color[] MinorColors => (Color[])minorColors.Clone();
+  }
+}
