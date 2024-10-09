b7aab0208d3862741d41173d488d7a0ad7d79d28
Naveen3521 <87026171+Naveen3521@users.noreply.github.com>
2024-10-09 21:03:53 +0530

    Update ColorUtility.cs

--- ColorUtility.cs
+++ ColorUtility.cs
@@ -1,1 +1,42 @@
+using System;
+
+namespace TelCo.ColorCoder
+{
+    public static class ColorUtility
+    {
+        public static ColorPair GetColorFromPairNumber(int pairNumber)
+        {
+            int minorSize = ColorMap.MinorColors.Length;
+            int majorSize = ColorMap.MajorColors.Length;
+
+            if (pairNumber < 1 || pairNumber > minorSize * majorSize)
+            {
+                throw new ArgumentOutOfRangeException($"Argument PairNumber: {pairNumber} is outside the allowed range.");
+            }
+
+            int zeroBasedPairNumber = pairNumber - 1;
+            int majorIndex = zeroBasedPairNumber / minorSize;
+            int minorIndex = zeroBasedPairNumber % minorSize;
+
+            return new ColorPair
+            {
+                MajorColor = ColorMap.MajorColors[majorIndex],
+                MinorColor = ColorMap.MinorColors[minorIndex]
+            };
+        }
+
+        public static int GetPairNumberFromColor(ColorPair pair)
+        {
+            int majorIndex = Array.IndexOf(ColorMap.MajorColors, pair.MajorColor);
+            int minorIndex = Array.IndexOf(ColorMap.MinorColors, pair.MinorColor);
+
+            if (majorIndex == -1 || minorIndex == -1)
+            {
+                throw new ArgumentException($"Unknown Colors: {pair}");
+            }
+
+            return (majorIndex * ColorMap.MinorColors.Length) + (minorIndex + 1);
+        }
+    }
+}
 
