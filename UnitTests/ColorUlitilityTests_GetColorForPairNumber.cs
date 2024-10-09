90f25dc3901946bfd725e26efa30d4eb9e0c8602
Naveen3521 <87026171+Naveen3521@users.noreply.github.com>
2024-10-09 22:46:10 +0530

    Create ColorUlitilityTests_GetColorForPairNumber.cs

---
+++ UnitTests/ColorUlitilityTests_GetColorForPairNumber.cs
@@ -0,0 +1,34 @@
+using System; 
+using System.Drawing;
+using Xunit;
+using TelCo.ColorCoder;
+
+namespace TelCo.ColorCoder.Tests
+{
+    public class ColorUtilityTests_GetColorFromPairNumber
+    {
+        [Theory]
+        [InlineData(1, Color.White, Color.Blue)]
+        [InlineData(5, Color.White, Color.SlateGray)]
+        [InlineData(10, Color.Black, Color.Green)]
+        [InlineData(18, Color.Yellow, Color.Green)]
+        [InlineData(25, Color.Violet, Color.SlateGray)]
+        public void GetColorFromPairNumber_ValidPairNumber_ReturnsCorrectColors(int pairNumber, Color expectedMajor, Color expectedMinor)
+        {
+            // Act
+            var result = ColorUtility.GetColorFromPairNumber(pairNumber);
+
+            // Assert
+            Assert.Equal(expectedMajor, result.MajorColor);
+            Assert.Equal(expectedMinor, result.MinorColor);
+        }
+
+        [Fact]
+        public void GetColorFromPairNumber_InvalidPairNumber_ThrowsArgumentOutOfRangeException()
+        {
+            // Act & Assert
+            var exception = Assert.Throws<ArgumentOutOfRangeException>(() => ColorUtility.GetColorFromPairNumber(0));
+            Assert.Contains("outside the allowed range", exception.Message);
+        }
+    }
+}
