9b1197146cf27187d10e93c0418b79f4768f6464
Naveen3521 <87026171+Naveen3521@users.noreply.github.com>
2024-10-10 16:20:22 +0530

    Update ColorUtility.cs

--- ColorUtility.cs
+++ ColorUtility.cs
@@ -1,6 +1,4 @@
 using System;
-using TelCo.ColorCoder;
-using System.Linq;
 
 namespace TelCo.ColorCoder
 {
@@ -8,31 +6,32 @@ namespace TelCo.ColorCoder
     {
         public static ColorPair GetColorFromPairNumber(int pairNumber)
         {
-            ValidatePairNumber(pairNumber);
-            int majorIndex = (pairNumber - 1) / ColorData.MinorColors.Count;
-            int minorIndex = (pairNumber - 1) % ColorData.MinorColors.Count;
+            int minorSize = ColorData.MinorColors.Length;
+            int majorSize = ColorData.MajorColors.Length;
+
+            if (pairNumber < 1 || pairNumber > minorSize * majorSize)
+            {
+                throw new ArgumentOutOfRangeException($"Argument PairNumber: {pairNumber} is outside the allowed range");
+            }
+
+            int zeroBasedPairNumber = pairNumber - 1;
+            int majorIndex = zeroBasedPairNumber / minorSize;
+            int minorIndex = zeroBasedPairNumber % minorSize;
+
             return new ColorPair(ColorData.MajorColors[majorIndex], ColorData.MinorColors[minorIndex]);
         }
 
         public static int GetPairNumberFromColor(ColorPair pair)
         {
-            int majorIndex = Array.IndexOf(ColorData.MajorColors.ToArray(), pair.MajorColor);
-            int minorIndex = Array.IndexOf(ColorData.MinorColors.ToArray(), pair.MinorColor);
+            int majorIndex = Array.IndexOf(ColorData.MajorColors, pair.MajorColor);
+            int minorIndex = Array.IndexOf(ColorData.MinorColors, pair.MinorColor);
 
             if (majorIndex == -1 || minorIndex == -1)
             {
                 throw new ArgumentException($"Unknown Colors: {pair}");
             }
 
-            return (majorIndex * ColorData.MinorColors.Count) + (minorIndex + 1);
-        }
-
-        private static void ValidatePairNumber(int pairNumber)
-        {
-            if (pairNumber < 1 || pairNumber > ColorData.MajorColors.Count * ColorData.MinorColors.Count)
-            {
-                throw new ArgumentOutOfRangeException($"Argument PairNumber: {pairNumber} is outside the allowed range");
-            }
+            return (majorIndex * ColorData.MinorColors.Length) + (minorIndex + 1);
         }
     }
 }
