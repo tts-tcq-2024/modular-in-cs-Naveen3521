221339ad0bb007a0904f36dfaa44c30bf94b01ef
Naveen3521 <87026171+Naveen3521@users.noreply.github.com>
2024-10-10 15:52:26 +0530

    Create ColorData.cs

---
+++ ColorData.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace TelCo.ColorCoder
+{
+    public static class ColorData
+    {
+        public static IReadOnlyList<Color> MajorColors { get; } =
+            new List<Color> { Color.White, Color.Red, Color.Black, Color.Yellow, Color.Violet }.AsReadOnly();
+
+        public static IReadOnlyList<Color> MinorColors { get; } =
+            new List<Color> { Color.Blue, Color.Orange, Color.Green, Color.Brown, Color.SlateGray }.AsReadOnly();
+    }
+}
