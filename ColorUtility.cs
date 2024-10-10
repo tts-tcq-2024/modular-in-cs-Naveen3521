ecba08021026c50eb7b414327d9277bad190b763
Naveen3521 <87026171+Naveen3521@users.noreply.github.com>
2024-10-10 17:21:51 +0530

    Update ColorUtility.cs

--- ColorUtility.cs
+++ ColorUtility.cs
@@ -4,31 +4,31 @@ namespace TelCo.ColorCoder
 {
     public static class ColorUtility
     {
-        int minorSize = ColorData.MinorColors.Length;
-        int majorSize = ColorData.MajorColors.Length;
+        private static readonly int minorSize = ColorData.MinorColorsArray.Length;
+        private static readonly int majorSize = ColorData.MajorColorsArray.Length;
+
         public static ColorPair GetColorFromPairNumber(int pairNumber)
         {
             ValidatePairNumber(pairNumber);
             int zeroBasedPairNumber = pairNumber - 1;
             int majorIndex = zeroBasedPairNumber / minorSize;
             int minorIndex = zeroBasedPairNumber % minorSize;
-            return new ColorPair(ColorData.MajorColors[majorIndex], ColorData.MinorColors[minorIndex]);
+            return new ColorPair(ColorData.MajorColorsArray[majorIndex], ColorData.MinorColorsArray[minorIndex]);
         }
 
         public static int GetPairNumberFromColor(ColorPair pair)
         {
-            int majorIndex = Array.IndexOf(ColorData.MajorColors, pair.MajorColor);
-            int minorIndex = Array.IndexOf(ColorData.MinorColors, pair.MinorColor);
+            int majorIndex = Array.IndexOf(ColorData.MajorColorsArray, pair.MajorColor);
+            int minorIndex = Array.IndexOf(ColorData.MinorColorsArray, pair.MinorColor);
             if (majorIndex == -1 || minorIndex == -1)
                 throw new ArgumentException($"Unknown Colors: {pair}");
-            return (majorIndex * ColorData.MinorColors.Length) + (minorIndex + 1);
-        }   
+            return (majorIndex * minorSize) + (minorIndex + 1);
+        }
+
         private static void ValidatePairNumber(int pairNumber)
         {
-            int minorSize = ColorData.MinorColors.Length;
-            int majorSize = ColorData.MajorColors.Length;
             if (pairNumber < 1 || pairNumber > minorSize * majorSize)
                 throw new ArgumentOutOfRangeException($"PairNumber is outside the allowed range or is null");
-        }  
+        }
     }
 }
