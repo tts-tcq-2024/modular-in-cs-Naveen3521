655639501f5bf6f2385d96ef26cef428bcfa07c4
Naveen3521 <87026171+Naveen3521@users.noreply.github.com>
2024-10-10 15:09:16 +0530

    Update ColorData.cs

--- ColorData.cs
+++ ColorData.cs
@@ -1,1 +1,18 @@
+using System.Drawing;
+
+namespace TelCo.ColorCoder
+{
+    public static class ColorData
+    {
+        public static readonly Color[] MajorColors = 
+        { 
+            Color.White, Color.Red, Color.Black, Color.Yellow, Color.Violet 
+        };
+
+        public static readonly Color[] MinorColors = 
+        { 
+            Color.Blue, Color.Orange, Color.Green, Color.Brown, Color.SlateGray 
+        };
+    }
+}
 
