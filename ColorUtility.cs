b29512608208454528b17de9b1baa63451cd4020
Naveen3521 <87026171+Naveen3521@users.noreply.github.com>
2024-10-10 16:58:54 +0530

    Update ColorUtility.cs

--- ColorUtility.cs
+++ ColorUtility.cs
@@ -4,20 +4,14 @@ namespace TelCo.ColorCoder
 {
     public static class ColorUtility
     {
+        int minorSize = ColorData.MinorColors.Length;
+        int majorSize = ColorData.MajorColors.Length;
         public static ColorPair GetColorFromPairNumber(int pairNumber)
         {
-            int minorSize = ColorData.MinorColors.Length;
-            int majorSize = ColorData.MajorColors.Length;
-
-            if (pairNumber < 1 || pairNumber > minorSize * majorSize)
-            {
-                throw new ArgumentOutOfRangeException($"Argument PairNumber: {pairNumber} is outside the allowed range");
-            }
-
+            ValidatePairNumber(pairNumber);
             int zeroBasedPairNumber = pairNumber - 1;
             int majorIndex = zeroBasedPairNumber / minorSize;
             int minorIndex = zeroBasedPairNumber % minorSize;
-
             return new ColorPair(ColorData.MajorColors[majorIndex], ColorData.MinorColors[minorIndex]);
         }
 
@@ -25,13 +19,16 @@ namespace TelCo.ColorCoder
         {
             int majorIndex = Array.IndexOf(ColorData.MajorColors, pair.MajorColor);
             int minorIndex = Array.IndexOf(ColorData.MinorColors, pair.MinorColor);
-
             if (majorIndex == -1 || minorIndex == -1)
-            {
                 throw new ArgumentException($"Unknown Colors: {pair}");
-            }
-
             return (majorIndex * ColorData.MinorColors.Length) + (minorIndex + 1);
-        }
+        }   
+        private static void ValidatePairNumber(int pairNumber)
+        {
+            int minorSize = ColorData.MinorColors.Length;
+            int majorSize = ColorData.MajorColors.Length;
+            if (pairNumber < 1 || pairNumber > minorSize * majorSize)
+                throw new ArgumentOutOfRangeException($"PairNumber is outside the allowed range or is null");
+        }  
     }
 }
